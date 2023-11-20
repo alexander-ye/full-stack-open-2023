24365f684b5411c77708b38b4f31cd78c146da03
Alexander Ye <alexander.ye7@gmail.com>
2023-11-19 21:34:50 -0500

    exercises 4.1-4.2

---
+++ part4/exercises/blogposts/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const config = require('./utils/config');
+const cors = require('cors');
+const blogsRouter = require('./controllers/blogs');
+const logger = require('./utils/logger');
+const mongoose = require('mongoose');
+
+mongoose.set('strictQuery', false);
+
+mongoose.connect(config.MONGODB_URI)
+  .then(() => logger.info('connected to MongoDB'))
+  .catch(error => logger.error(error)); 
+
+app.use(cors());
+app.use(express.static('dist'));
+app.use(express.json());
+app.use('/api/blogs', blogsRouter);
+
+module.exports = app;
---
+++ part4/exercises/blogposts/controllers/blogs.js
@@ -0,0 +1,58 @@
+const blogsRouter = require('express').Router();
+const Blog = require('../models/blog');
+const logger = require('./utils/logger');
+
+blogsRouter.get('/', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs);
+    });
+});
+
+blogsRouter.get('/:id', (request, response) => {
+  Blog
+    .findById(request.params.id)
+    .then(blog => {
+      if (blog) {
+        response.json(blog);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch(error => {
+      console.error(error);
+      response.status(400).send({ error: 'something went wrong' });
+    });
+});
+
+blogsRouter.post('/', (request, response) => {
+  const {body} = request;
+
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes
+  });
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result);
+    })
+    .catch(error => {
+      logger.error(error);
+      response.status(400).send({ error: 'something went wrong' });
+    })
+});
+
+blogsRouter.delete('/:id', (request, response, next) => {
+  Blog.findByIdAndDelete(request.params.id)
+    .then(() => {
+      response.status(204).end();
+    })
+    .catch(error => next(error))
+});
+
+module.exports = blogsRouter;
