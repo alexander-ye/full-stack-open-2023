4e6a7c751f73ebeecf99b0de2aed2b0d2bcebb46
alexander-ye <alexander.ye7@gmail.com>
2023-11-24 14:50:02 -0500

    exercise 4.13, delete posts and refactor tests

--- part4/exercises/blogposts/tests/blog_api.test.js
+++ part4/exercises/blogposts/tests/blog_api.test.js
@@ -5,7 +5,6 @@ const app = require('../app');
 const api = supertest(app);
 
 const Blog = require('../models/blog');
-const listWithOneBlog = require('./dummy_data').dummyListWithOneBlog;
 const blogs = require('./dummy_data').dummyBlogs;
 
 beforeEach(async () => {
@@ -13,127 +12,134 @@ beforeEach(async () => {
   await Blog.deleteMany({});
 
   // Save blogs stored in dummy data initial blogs array to database
-  const blogObjects = blogs.map(blog => new Blog(blog));
-  const promises = blogObjects.map(blog => blog.save());
-  await Promise.all(promises);
+  await Blog.insertMany(blogs);
 });
+
+describe('fetching blogs', () => {
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/);
+  })
+  
+  test('all blogs are returned', async () => {
+    const response = await api.get('/api/blogs');
+  
+    expect(response.body).toHaveLength(blogs.length);
+  });
+})
+
+
+describe('creating blog posts', () => {
+  test('a valid blog can be added', async () => {
+    const newBlog = {
+      title: "General Kenobi",
+      author: "General Grievous",
+      url: "/hello-there",
+      likes: 0
+    }
+  
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+    
+    const blogsAtEnd = await Blog.find({});
+    expect(blogsAtEnd).toHaveLength(blogs.length + 1);
+  
+    const blogContents = blogsAtEnd.map(({title, author}) => {
+      return {title, author}});
+    expect(blogContents).toContainEqual({title: newBlog.title, author: newBlog.author});
+  });
+  
+  test('blog without title is not added', async () => {
+    const newBlog = {
+      author: "Anakin Skywalker",
+      url: "/anakin",
+      likes: 1
+    }
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(400);
+  });
+  
+  test('blog without url is not added', async () => {
+    const newBlog = {
+      title: "The Chosen One",
+      author: "Anakin Skywalker",
+      likes: 1
+    }
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(400);
+  });
+  
+  test('blog without title and url is not added', async () => {
+    const newBlog = {
+      author: "Anakin Skywalker",
+      likes: 1
+    }
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(400);
+  });
+
+  test('new post with missing likes field defaults to 0', async () => {
+    const newBlog = {
+      title: "Yoda",
+      author: "My name is",
+      url: "/dagobah"
+    }
+  
+    const storedBlog = await api
+      .post('/api/blogs')
+      .send(newBlog)
+  
+    expect(storedBlog?._body?._id).toBeDefined();
+    const newBlogId = storedBlog._body._id;
+  
+    const returnedBlog = await Blog.find({_id: newBlogId});
+  
+    expect(returnedBlog).toHaveLength(1);
+    expect(returnedBlog[0].likes).toBe(0);
+  })
+  
+  test('blogs have unique identifiers named id', async () => {
+    const blogs = await Blog.find({});
+  
+    const blogIds = blogs.map(({id}) => {
+      expect(id).toBeDefined();
+      return id
+    });
+    expect([...new Set(blogIds)]).toHaveLength(blogs.length);
+  });
+})
+
+
+describe('deleting blog posts', () => {
+  test('can delete blog', async () => {
+    const blogsAtStart = await Blog.find({});
+    const blogToDelete = blogsAtStart[0];
+  
+    await api
+      .delete(`/api/blogs/${blogToDelete.id}`)
+      .expect(204);
+    
+    const blogsAtEnd = await Blog.find({});
+    expect(blogsAtEnd).toHaveLength(blogs.length - 1);
+  
+    const blogContents = blogsAtEnd.map(({title, author}) => {
+      return {title, author}});
+    expect(blogContents).not.toContainEqual({title: blogToDelete.title, author: blogToDelete.author});
+  });
+})
+
 
 afterAll(async () => {
   await mongoose.connection.close();
 });
-
-test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/);
-})
-
-test('all blogs are returned', async () => {
-  const response = await api.get('/api/blogs');
-
-  expect(response.body).toHaveLength(blogs.length);
-});
-
-test('a valid blog can be added', async () => {
-  const newBlog = {
-    title: "General Kenobi",
-    author: "General Grievous",
-    url: "/hello-there",
-    likes: 0
-  }
-
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
-  
-  const blogsAtEnd = await Blog.find({});
-  expect(blogsAtEnd).toHaveLength(blogs.length + 1);
-
-  const blogContents = blogsAtEnd.map(({title, author}) => {
-    return {title, author}});
-  expect(blogContents).toContainEqual({title: newBlog.title, author: newBlog.author});
-});
-
-test('blog without title is not added', async () => {
-  const newBlog = {
-    author: "Anakin Skywalker",
-    url: "/anakin",
-    likes: 1
-  }
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(400);
-});
-
-test('blog without url is not added', async () => {
-  const newBlog = {
-    title: "The Chosen One",
-    author: "Anakin Skywalker",
-    likes: 1
-  }
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(400);
-});
-
-test('blog without title and url is not added', async () => {
-  const newBlog = {
-    author: "Anakin Skywalker",
-    likes: 1
-  }
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(400);
-});
-
-test('blogs have unique identifiers named id', async () => {
-  const blogs = await Blog.find({});
-
-  const blogIds = blogs.map(({id}) => {
-    expect(id).toBeDefined();
-    return id
-  });
-  expect([...new Set(blogIds)]).toHaveLength(blogs.length);
-});
-
-test('can delete blog', async () => {
-  const blogsAtStart = await Blog.find({});
-  const blogToDelete = blogsAtStart[0];
-
-  await api
-    .delete(`/api/blogs/${blogToDelete.id}`)
-    .expect(204);
-  
-  const blogsAtEnd = await Blog.find({});
-  expect(blogsAtEnd).toHaveLength(blogs.length - 1);
-
-  const blogContents = blogsAtEnd.map(({title, author}) => {
-    return {title, author}});
-  expect(blogContents).not.toContainEqual({title: blogToDelete.title, author: blogToDelete.author});
-});
-
-test('new post with missing likes field defaults to 0', async () => {
-  const newBlog = {
-    title: "Yoda",
-    author: "My name is",
-    url: "/dagobah"
-  }
-
-  const storedBlog = await api
-    .post('/api/blogs')
-    .send(newBlog)
-
-  expect(storedBlog?._body?._id).toBeDefined();
-  const newBlogId = storedBlog._body._id;
-
-  const returnedBlog = await Blog.find({_id: newBlogId});
-
-  expect(returnedBlog).toHaveLength(1);
-  expect(returnedBlog[0].likes).toBe(0);
-})
