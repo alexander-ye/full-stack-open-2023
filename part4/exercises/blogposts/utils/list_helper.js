c4b85a04f11b78dd739fbabc1625c594c41b94f5
alexander-ye <alexander.ye7@gmail.com>
2023-11-24 10:39:04 -0500

    exercise 4.4

--- part4/exercises/blogposts/utils/list_helper.js
+++ part4/exercises/blogposts/utils/list_helper.js
@@ -2,6 +2,16 @@ const dummy = (blogs) => {
   return 1;
 }
 
+const totalLikes = (blogs) => {
+  return blogs.reduce((sum, blog) => {
+    if (!blog.likes) return sum;
+    return sum + blog.likes;
+  }, 0);
+}
+
+
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
