1b6e7bea1c9d251064a9eb7c95ae6853adbb6507
alexander-ye <alexander.ye7@gmail.com>
2023-11-24 18:52:32 -0500

    exercise 4.15

--- part4/exercises/blogposts/app.js
+++ part4/exercises/blogposts/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const config = require('./utils/config');
 const cors = require('cors');
+const usersRouter = require('./controllers/users');
 const blogsRouter = require('./controllers/blogs');
 const logger = require('./utils/logger');
 const mongoose = require('mongoose');
@@ -15,6 +16,7 @@ mongoose.connect(config.MONGODB_URI)
 app.use(cors());
 app.use(express.static('dist'));
 app.use(express.json());
+app.use('/api/users', usersRouter);
 app.use('/api/blogs', blogsRouter);
 
 module.exports = app;
---
+++ part4/exercises/blogposts/tests/user_api.test.js
@@ -0,0 +1,63 @@
+const bcrypt = require('bcrypt')
+const User = require('../models/user')
+const helper = require('./helpers')
+
+const supertest = require('supertest');
+const app = require('../app');
+
+const api = supertest(app);
+
+describe('when there is initially one user in db', () => {
+  beforeEach(async () => {
+    await User.deleteMany({})
+
+    const passwordHash = await bcrypt.hash('sekret', 10)
+    const user = new User({ username: 'root', name: 'Superuser', passwordHash })
+
+    await user.save()
+  })
+
+  test('creation succeeds with a fresh username', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'gkenobi',
+      name: 'Oobie Doobie Banoobie',
+      password: 'master',
+    }
+
+    await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const usersAtEnd = await helper.usersInDb()
+    expect(usersAtEnd).toHaveLength(usersAtStart.length + 1)
+
+    const usernames = usersAtEnd.map(u => u.username)
+    expect(usernames).toContain(newUser.username)
+  })
+  
+  // The app crashes if we try this...
+  test.skip('creation fails with proper statuscode and message if username already taken', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'root',
+      name: 'Master Skywalker',
+      password: 'anakin',
+    }
+
+    const result = await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(400)
+      .expect('Content-Type', /application\/json/)
+
+    expect(result._body).toContain('expected `username` to be unique')
+
+    const usersAtEnd = await helper.usersInDb()
+    expect(usersAtEnd).toEqual(usersAtStart)
+  })
+})
