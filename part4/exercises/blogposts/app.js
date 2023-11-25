e02ae72a684dab9d11eb2207665ac1725af00953
alexander-ye <alexander.ye7@gmail.com>
2023-11-24 19:34:55 -0500

    middleware for aprt 4

--- part4/exercises/blogposts/app.js
+++ part4/exercises/blogposts/app.js
@@ -5,10 +5,13 @@ const cors = require('cors');
 const usersRouter = require('./controllers/users');
 const blogsRouter = require('./controllers/blogs');
 const loginRouter = require('./controllers/login')
+const middleware = require('./utils/middleware');
 const logger = require('./utils/logger');
 const mongoose = require('mongoose');
 
 mongoose.set('strictQuery', false);
+
+logger.info('connecting to', config.MONGODB_URI);
 
 mongoose.connect(config.MONGODB_URI)
   .then(() => logger.info('connected to MongoDB'))
@@ -17,8 +20,13 @@ mongoose.connect(config.MONGODB_URI)
 app.use(cors());
 app.use(express.static('dist'));
 app.use(express.json());
+app.use(middleware.requestLogger);
+
 app.use('/api/users', usersRouter);
 app.use('/api/blogs', blogsRouter);
 app.use('/api/login', loginRouter);
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
 
 module.exports = app;
