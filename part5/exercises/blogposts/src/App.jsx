956bbfaaa70df053b48265fc0b402e1375a6f142
alexander-ye <alexander.ye7@gmail.com>
2023-11-24 22:59:16 -0500

    exercise 5.1

--- part5/exercises/blogposts/src/App.jsx
+++ part5/exercises/blogposts/src/App.jsx
@@ -1,9 +1,11 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -14,11 +16,66 @@ const App = () => {
   return (
     <div>
       <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
+      
+      {user === null 
+        ? <LoginForm user={user} setUser={setUser} />
+        : <>
+          <p>{user.name} logged in</p>
+          {blogs.map(blog =>
+            <Blog key={blog.id} blog={blog} />
       )}
+        </>}
     </div>
   )
 }
 
 export default App
+
+const LoginForm = ({user, setUser}) => {
+  const [username, setUsername] = useState('') 
+  const [password, setPassword] = useState('') 
+  
+  const handleLogin = async (event) => {
+    // Backend expects token verifying user's identity to be sent in the Authorization header of note
+    event.preventDefault()
+    try {
+      console.log('logging in with', username, password)
+      const user = await loginService.login({
+        username, password,
+      })
+
+      // Save the user to local storage
+      window.localStorage.setItem(
+        'loggedNoteappUser', JSON.stringify(user)
+      ) 
+      // On successful login
+      // Empty form fields, save server response (token + user details) to application state
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      console.error(exception);
+    }
+  }
+  return  <form onSubmit={handleLogin}>
+  <div>
+    username
+      <input
+      type="text"
+      value={username}
+      name="Username"
+      onChange={({ target }) => setUsername(target.value)}
+    />
+  </div>
+  <div>
+    password
+      <input
+      type="password"
+      value={password}
+      name="Password"
+      onChange={({ target }) => setPassword(target.value)}
+    />
+  </div>
+  <button type="submit">login</button>
+</form>
+}
---
+++ part5/exercises/blogposts/src/services/login.js
@@ -0,0 +1,10 @@
+import axios from 'axios'
+const baseUrl = 'http://localhost:3001'
+const baseSlug = '/api/login'
+
+const login = async credentials => {
+  const response = await axios.post(`${baseUrl}${baseSlug}`, credentials)
+  return response.data
+}
+
+export default { login }
